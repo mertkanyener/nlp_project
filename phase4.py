4b007e6b1c2c3d26de7249978d788a86b3fc6d97
mertkanyener <mertkanyener@outlook.com>
2018-05-22 14:13:14 +0300

    NB classifier successfully implemented and tested on dataset. Accuracy = aprrox. %67

--- phase4.py
+++ phase4.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import random
+import numpy as np
+import gzip
 
 
 from nltk.corpus import stopwords
@@ -7,33 +8,101 @@ from nltk import *
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA, TruncatedSVD, KernelPCA
 
 
-df = pd.read_csv('amazon_d.csv', header=None)
+def parse(path):
+  g = gzip.open(path, 'rb')
+  for l in g:
+    yield eval(l)
 
-df.columns = [col for col in df.iloc[0, :]]
 
-reviews_ratings = df.loc[:, ('reviews.text', 'reviews.rating')]  # get reviews and ratings together
-df_rr_cleared = reviews_ratings.dropna(axis=0)  # delete rows with the missing values
-df_rr = df_rr_cleared.iloc[1:, :]  # do not include first row which is: review.text review.rating
-df_rr = df_rr.reset_index(drop=True)
-X = df_rr.loc[:, 'reviews.text']
-y = df_rr.loc[:, 'reviews.rating']
-length = df_rr.count()[1]  # row count of dataframe
+def getDF(path):
+  i = 0
+  df = {}
+  for d in parse(path):
+    df[i] = d
+    i += 1
+  return pd.DataFrame.from_dict(df, orient='index')
 
-# Find the stem of words in text
 
-#tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2',
-#                        encoding='latin-1', ngram_range=(1, 2), stop_words='english')
-#features = tfidf.fit_transform(X).toarray()
-count_vect = CountVectorizer()
-tfidf_transformer = TfidfTransformer()
-X_counts = count_vect.fit_transform(X)
-X_tfidf = tfidf_transformer.fit_transform(X_counts)
+def read_data(filename):
+    df = getDF(filename)
+    X = df.loc[:10000, 'reviewText'].values
+    y = df.loc[:10000, 'overall'].values
 
-X_train, X_test, y_train, y_test = train_test_split(X_tfidf, y, test_size=0.2, random_state=6, stratify=y)
+    return X, y
 
-NB = MultinomialNB().fit(X_train, y_train)
-y_pred = NB.predict(X_test)
-print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
+
+def text_representation(X):
+
+    count_vect = CountVectorizer()
+    tfidf_transformer = TfidfTransformer()
+    X_counts = count_vect.fit_transform(X)
+    X_tfidf = tfidf_transformer.fit_transform(X_counts)
+
+    return X_tfidf
+
+
+def run_classifiers(X, y, random_state, pipelines):
+
+    pipe_nb, pipe_lr, pipe_svm, pipe_forest, pipe_knn = pipelines
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state, stratify=y)
+
+    pipe_nb.fit(X_train, y_train)
+    y_pred = pipe_nb.predict(X_test)
+    print('Naive Bayes Accuracy: %.3f' % accuracy_score(y_test, y_pred))
+
+    pipe_forest.fit(X_train, y_train)
+    y_pred = pipe_forest.predict(X_test)
+    print('Random Forest Accuracy: %.3f' % accuracy_score(y_test, y_pred))
+
+    pipe_lr.fit(X_train, y_train)
+    y_pred = pipe_lr.predict(X_test)
+    print('Logistic Regression Accuracy: %.3f' % accuracy_score(y_test, y_pred))
+
+    pipe_svm.fit(X_train, y_train)
+    y_pred = pipe_svm.predict(X_test)
+    print('SVM Accuracy: %.3f' % accuracy_score(y_test, y_pred))
+
+    pipe_knn.fit(X_train, y_train)
+    y_pred = pipe_knn.predict(X_test)
+    print('KNN Accuracy: %.3f' % accuracy_score(y_test, y_pred))
+
+
+def make_pipelines():
+
+    pipe_nb = make_pipeline(
+                            MultinomialNB())
+
+    pipe_lr = make_pipeline(
+                            LogisticRegression(random_state=1))
+
+    pipe_svm = make_pipeline(
+                            SVC(random_state=1))
+
+    pipe_forest = make_pipeline(
+                                #TruncatedSVD(n_components=2),
+                                RandomForestClassifier(n_estimators=100, random_state=1))
+
+    pipe_knn = make_pipeline(
+                             #TruncatedSVD(n_components=2),
+                             KNeighborsClassifier(n_neighbors=5))
+
+    pipelines = pipe_nb, pipe_lr, pipe_svm, pipe_forest, pipe_knn
+
+    return pipelines
+
+
+X, y = read_data('reviews_Video_Games.json.gz')
+X_bow = text_representation(X)
+pipelines = make_pipelines()
+run_classifiers(X_bow, y, 4, pipelines)
+
